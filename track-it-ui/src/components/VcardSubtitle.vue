312c6b28852b8afde514b55c1b1fe862861d17e9
Łukasz Latusik <latusikl@gmail.com>
2020-10-25 10:13:30 +0100

    [UI] Refactor home component

---
+++ track-it-ui/src/components/VcardSubtitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="d-flex flex-row justify-start pl-15">
+    <div class="text-h4 text-center pt-15 pr-5">
+      {{ subtitle }}
+    </div>
+    <v-img
+      max-height="200"
+      max-width="150"
+      v-bind:src="require('../assets/' + imgPath)"
+    ></v-img>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class VcardSubtitle extends Vue {
+  @Prop(String) subtitle!: string;
+  @Prop(String) imgPath!: string;
+}
+</script>
--- track-it-ui/src/views/Home.vue
+++ track-it-ui/src/views/Home.vue
@@ -1,44 +1,30 @@
 <template>
-  <v-container>
+  <v-container class="mb-15">
     <Welcome></Welcome>
     <v-card class="background d-flex flex-column" flat tile>
       <div class="text-h2 pa-10 mb-10">Try it out!</div>
-      <div class="d-flex flex-row justify-start pl-15">
-        <div class="text-h4 text-center pt-15 pr-5">
-          Create account
-        </div>
-        <v-img
-          max-height="200"
-          max-width="150"
-          src="@/assets/account.svg"
-        ></v-img>
-      </div>
-      <div class="d-flex flex-row justify-end pr-15">
-        <div class="text-h4 text-center pt-15 pr-5">
-          Add device
-        </div>
-        <v-img
-          max-height="200"
-          max-width="150"
-          src="@/assets/device.svg"
-        ></v-img>
-      </div>
-      <div class=" d-flex flex-row justify-start pl-15">
-        <div class="text-h4 text-center pt-15 pr-5">
-          Check connection
-        </div>
-        <v-img
-          max-height="150"
-          max-width="200"
-          src="@/assets/check.svg"
-        ></v-img>
-      </div>
-      <div class="d-flex flex-row justify-end pr-15">
-        <div class="text-h4 text-center pt-15 pr-5">
-          All set!
-        </div>
-        <v-img max-height="150" max-width="200" src="@/assets/done.svg"></v-img>
-      </div>
+
+      <VcardSubtitle
+        subtitle="Create account"
+        img-path="account.svg"
+      ></VcardSubtitle>
+
+      <VcardSubtitle
+        class="justify-end"
+        subtitle="Add device"
+        img-path="device.svg"
+      ></VcardSubtitle>
+
+      <VcardSubtitle
+        subtitle="Check connection"
+        img-path="check.svg"
+      ></VcardSubtitle>
+
+      <VcardSubtitle
+        class="justify-end"
+        subtitle="All set!"
+        img-path="done.svg"
+      ></VcardSubtitle>
     </v-card>
   </v-container>
 </template>
@@ -46,10 +32,12 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Welcome from "@/components/Welcome.vue";
+import VcardSubtitle from "@/components/VcardSubtitle.vue";
 
 @Component({
   components: {
-    Welcome
+    Welcome,
+    VcardSubtitle
   }
 })
 export default class Home extends Vue {}
