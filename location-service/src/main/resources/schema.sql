270e56eb4c3ee44e08b887eb3fb7471c672d36d5
Łukasz Latusik <latusikl@gmail.com>
2020-10-28 11:47:15 +0100

    [LS] Add tables to database schema

--- location-service/src/main/resources/schema.sql
+++ location-service/src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS location_data (
 	location_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	device_id VARCHAR (30) NOT NULL,
@@ -13,4 +15,17 @@ CREATE TABLE IF NOT EXISTS device_info (
 	message VARCHAR (300) NOT NULL,
 	server_date_time TIMESTAMP,
 	info_level VARCHAR (8)
+);
+
+CREATE TABLE IF NOT EXISTS user_data (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v1 (),
+    user_email VARCHAR (100) NOT NULL,
+    password VARCHAR (250) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_devices (
+    device_id VARCHAR (30) PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES user_data(user_id) ON DELETE CASCADE,
+    device_name VARCHAR (25) NOT NULL,
+    device_status VARCHAR (20) NOT NULL
 );
