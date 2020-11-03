bc83e51b7e87d06ab725fe4edd1fceb8cdedfc87
Łukasz Latusik <latusikl@gmail.com>
2020-11-03 22:17:11 +0100

    [UI] Add base of timestamp choice for location from interval

---
+++ track-it-ui/src/components/location/DateRangeStepper.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-container class="container-width">
+    <v-stepper v-model="stepNumber">
+      <v-stepper-header>
+        <v-stepper-step :complete="stepNumber > 1" step="1">
+          Choose start timestamp
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step :complete="stepNumber > 2" step="2">
+          Choose end timestamp
+        </v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-10" color="secondary">
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-row class="justify-center text-h6">
+                    Choose date
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row class="justify-center text-h6">
+                    Choose time
+                  </v-row>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+              <v-row align="center" justify="center">
+                <v-col>
+                  <v-row justify="center">
+                    <v-date-picker v-model="startDatePicker"></v-date-picker>
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row justify="center">
+                    <v-time-picker
+                      format="24hr"
+                      v-model="startTime"
+                    ></v-time-picker>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+
+          <v-btn color="primary" @click="stepNumber = 2">
+            Next
+          </v-btn>
+          <v-btn text @click="stepNumber = 1">
+            Cancel
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-10" color="secondary">
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-row class="justify-center text-h6">
+                    Choose date
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row class="justify-center text-h6">
+                    Choose time
+                  </v-row>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+              <v-row align="center" justify="center">
+                <v-col>
+                  <v-row justify="center">
+                    <v-date-picker v-model="endDatePicker"></v-date-picker>
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row justify="center">
+                    <v-time-picker
+                      format="24hr"
+                      v-model="endTime"
+                    ></v-time-picker>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+
+          <v-btn color="primary" @click="stepNumber = 1">
+            Get data
+          </v-btn>
+          <v-btn text @click="stepNumber = 1">
+            Back
+          </v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class DeviceRangeStepper extends Vue {
+  private stepNumber = 1;
+  private startDatePicker = this.getCurrentDate();
+  private startTime = this.getCurrentTime();
+  private endDatePicker = this.getCurrentDate();
+  private endTime = this.getCurrentTime();
+
+  private getCurrentDate(): string {
+    return new Date().toISOString().substr(0, 10);
+  }
+
+  private getCurrentTime(): string {
+    return new Date().toISOString().substr(11, 5);
+  }
+}
+</script>
--- track-it-ui/src/views/devices/DevicesInterval.vue
+++ track-it-ui/src/views/devices/DevicesInterval.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="background fill-height d-flex flex-row">
     <device-submenu></device-submenu>
-    <v-card class="flat flex-grow-1 red rounded-0"
-      >Hello Device Interval</v-card
-    >
+    <v-container>
+      <v-row>
+        <vcard-subtitle
+          class="justify-start"
+          img-path="logo.svg"
+          subtitle="Check location from time interval"
+        ></vcard-subtitle>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row>
+        <date-range-stepper></date-range-stepper>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import DeviceSubmenu from "@/components/devices/DeviceSubmenu.vue";
+import DateRangeStepper from "@/components/location/DateRangeStepper.vue";
+import VcardSubtitle from "@/components/home/VcardSubtitle.vue";
 
 @Component({
   components: {
-    DeviceSubmenu
+    DateRangeStepper,
+    DeviceSubmenu,
+    VcardSubtitle
   }
 })
 export default class DevicesInterval extends Vue {}
