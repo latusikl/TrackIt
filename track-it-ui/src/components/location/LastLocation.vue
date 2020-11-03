8fb1ee9187a78e70c7d12a8eeefc358e536fec91
Łukasz Latusik <latusikl@gmail.com>
2020-11-03 21:22:02 +0100

    [UI] Add refresh button

--- track-it-ui/src/components/location/LastLocation.vue
+++ track-it-ui/src/components/location/LastLocation.vue
@@ -6,6 +6,15 @@
       :alert-type="alertType"
       @invisible-event="makeInvisible"
     ></alert>
+    <v-icon
+      large
+      color="black"
+      class="float-right mt-2 mr-2"
+      @click="updateLocationRefreshClick"
+      v-if="isMapVisible"
+    >
+      mdi-refresh
+    </v-icon>
     <last-location-info
       v-if="isMapVisible"
       :latitude="latitude"
@@ -29,6 +38,7 @@ import LastLocationInfo from "@/components/location/LastLocationInfo.vue";
 import LocationService from "@/sevices/LocationService";
 import { LastLocationDto } from "@/dto/LastLocationDto";
 import Alert from "@/components/Alert.vue";
+import { transformGeom2D } from "ol/geom/SimpleGeometry";
 
 @Component({
   components: { Alert, LastLocationInfo, GeoJsonMap }
@@ -65,6 +75,10 @@ export default class DevicesLast extends Vue {
         console.warn(reason);
         this.showError();
       });
+  }
+  private updateLocationRefreshClick() {
+    this.makeInvisible();
+    this.updateLocation(this.deviceId);
   }
 
   private assignData(responseData: LastLocationDto) {
