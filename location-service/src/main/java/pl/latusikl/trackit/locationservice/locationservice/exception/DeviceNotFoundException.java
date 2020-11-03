3457fb16d4973feeb975dd2fde0eaafe06e5d44d
Łukasz Latusik <latusikl@gmail.com>
2020-11-03 19:25:40 +0100

    [LS] Change response code when DeviceNotFoundException

--- location-service/src/main/java/pl/latusikl/trackit/locationservice/locationservice/exception/DeviceNotFoundException.java
+++ location-service/src/main/java/pl/latusikl/trackit/locationservice/locationservice/exception/DeviceNotFoundException.java
@@ -3,11 +3,13 @@ package pl.latusikl.trackit.locationservice.locationservice.exception;
 import org.springframework.http.HttpStatus;
 
 public class DeviceNotFoundException extends AbstractRuntimeException {
+	private static final HttpStatus HTTP_STATUS =  HttpStatus.NOT_FOUND;
+
 	public DeviceNotFoundException(final String message, final String additionalInformation) {
-		super(message, HttpStatus.BAD_REQUEST, additionalInformation);
+		super(message, HTTP_STATUS, additionalInformation);
 	}
 
 	public DeviceNotFoundException(final String message) {
-		super(message, HttpStatus.BAD_REQUEST, "");
+		super(message, HTTP_STATUS, "");
 	}
 }
