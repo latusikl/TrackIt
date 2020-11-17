09830e468a94de1bdd9c2e0277973877efbe3557
Łukasz Latusik <latusikl@gmail.com>
2020-11-17 18:36:33 +0100

    [LS] Modify DB schama and yaml properties

--- location-service/src/test/java/pl/latusikl/trackit/locationservice/locationservice/LocationServiceApplicationTests.java
+++ location-service/src/test/java/pl/latusikl/trackit/locationservice/locationservice/LocationServiceApplicationTests.java
@@ -3,11 +3,11 @@ package pl.latusikl.trackit.locationservice.locationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LocationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class LocationServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
