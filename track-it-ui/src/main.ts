496d22d99aa5bd40a37c1cefb42c1ff9844ec66b
Łukasz Latusik <latusikl@gmail.com>
2020-11-17 18:37:07 +0100

    Update docker-compose

--- track-it-ui/src/main.ts
+++ track-it-ui/src/main.ts
@@ -5,7 +5,7 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 
 import "@/assets/style/global.css";
 
